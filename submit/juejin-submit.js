9cd8dfec423c07475ddb2fb6609e91c0b22512ad
yangchaohao <yangchaohao@taou.com>
2022-07-14 10:11:01 +0800

    fix:美化邮件

--- submit/juejin-submit.js
+++ submit/juejin-submit.js
@@ -13,11 +13,9 @@ const emailInfo = {
 };
 
 const formatObjToHtml = (obj) => {
-  return `<div style="background: #aaaaaa77">${JSON.stringify(
-    obj,
-    null,
-    "&nbsp;"
-  ).replace(/\n/g, "<br/>")}</div>`;
+  return `<div style="background: #aaaaaa55">${JSON.stringify(obj, null, 2)
+    .replace(/\u0020/g, "&nbsp;")
+    .replace(/\n/g, "<br/>")}</div>`;
 };
 
 /**
